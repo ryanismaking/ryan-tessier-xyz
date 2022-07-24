c3e410abd142ae6a617ca3453ca195f7146b584f
Ryan Tessier <tessier.ryan.m@gmail.com>
2022-07-24 15:17:52 +1000

    Update content to include published article

--- components/Author.tsx
+++ components/Author.tsx
@@ -1,13 +1,15 @@
 import { FunctionComponent } from "react";
-import styles from "../styles/Author.module.css"
+import styles from "../styles/Author.module.css";
+import avatar from "../public/ryan.jpg";
+import Image from "next/image";
 
 const Author: FunctionComponent = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.avatar} />
+      <Image src={avatar} className={styles.avatar} width={64} height={64} />
       <div className={styles.name}>Ryan Tessier</div>
     </div>
-  )
-}
+  );
+};
 
-export default Author
+export default Author;
