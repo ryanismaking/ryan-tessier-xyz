376fd7c52a697566dd9ba195336fc0b00e3be1a4
Ryan Tessier <tessier.ryan.m@gmail.com>
2022-07-26 21:38:30 +1000

    Fix the email capture form

--- components/EmailCapture.tsx
+++ components/EmailCapture.tsx
@@ -1,7 +1,46 @@
-import { FunctionComponent } from "react";
+import {
+  ChangeEventHandler,
+  FormEventHandler,
+  FunctionComponent,
+  useState,
+} from "react";
 import styles from "../styles/EmailCapture.module.css";
 
+const encode = (data: { [key: string]: string }) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
+
 const EmailCapture: FunctionComponent = () => {
+  const [firstName, setFirstName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+
+  const subscribe: FormEventHandler = (event) => {
+    event.preventDefault();
+
+    const formData = {
+      "form-name": "subscribe",
+      "first-name": firstName,
+      "email-address": emailAddress,
+    };
+    const encodedFormData = encode(formData);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encodedFormData,
+    })
+      .then(() => alert("You've been subscribed!"))
+      .catch((error) => alert(error));
+  };
+
+  const updateFirstName: ChangeEventHandler<HTMLInputElement> = (event) =>
+    setFirstName(event.target.value);
+
+  const updateEmailAddress: ChangeEventHandler<HTMLInputElement> = (event) =>
+    setEmailAddress(event.target.value);
+
   return (
     <div className={styles.section}>
       <h2 className={styles.title}>Level up your product design craft</h2>
@@ -13,17 +52,37 @@ const EmailCapture: FunctionComponent = () => {
       <form
         name="subscribe"
         method="POST"
-        className={styles.form}
         data-netlify="true"
+        onSubmit={subscribe}
+        className={styles.form}
       >
+        <input type="hidden" name="form-name" value="subscribe" />
         <div className={styles.textFields}>
           <div className={styles.textFieldContainer}>
-            <label className={styles.textFieldLabel}>First name</label>
-            <input type="text" name="first-name" className={styles.textField} />
+            <label htmlFor="first-name" className={styles.textFieldLabel}>
+              First name
+            </label>
+            <input
+              type="text"
+              id="first-name"
+              name="first-name"
+              value={firstName}
+              onChange={updateFirstName}
+              className={styles.textField}
+            />
           </div>
           <div className={styles.textFieldContainer}>
-            <label className={styles.textFieldLabel}>Email</label>
-            <input type="text" name="email" className={styles.textField} />
+            <label htmlFor="email-address" className={styles.textFieldLabel}>
+              Email address
+            </label>
+            <input
+              type="email"
+              id="email-address"
+              name="email-address"
+              value={emailAddress}
+              onChange={updateEmailAddress}
+              className={styles.textField}
+            />
           </div>
         </div>
         <button type="submit" className={styles.button}>
